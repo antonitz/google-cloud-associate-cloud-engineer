b61f2a18feb619a732709f57fb80c0e57d3149fd
Antoni Tzavelas <antoni@Antonis-MacBook-Pro.local>
2020-11-08 11:48:47 -0500

    edit script

--- 07-Compute-Engine/02_metadata_and_startup_scripts/bowtie-startup-final.sh
+++ 07-Compute-Engine/02_metadata_and_startup_scripts/bowtie-startup-final.sh
@@ -10,6 +10,6 @@ cat <<EOF> /var/www/html/index.html
 <html><body><h1>Aaaand.... Success!</h1>
 <p>My machine name is <span style="color: #3BA959">$NAME</span>$NAME<span> and I serve the <span style="color: #3BA959">$ENV</span> environment.</p>
 <p>I live comfortably in the <span style="color: #5383EC">$ZONE</span> datacenter and proudly serve Tony Bowtie on the <span style="color: #D85040">$PROJECT</span> project.</p>
-<p><img src="https://storage.googleapis.com/bowtie-site/tony-bowtie.svg" alt="Tony Bowtie"></p>
+<p><img src="https://storage.googleapis.com/tony-bowtie-pics/tony-bowtie.svg" alt="Tony Bowtie"></p>
 </body></html>
 EOF
