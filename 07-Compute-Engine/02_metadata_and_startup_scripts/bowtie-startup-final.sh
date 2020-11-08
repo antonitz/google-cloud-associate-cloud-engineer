128e31db56986036330a8b8d1f5f14e5d898d27e
Antoni Tzavelas <antoni@Antonis-MacBook-Pro.local>
2020-11-08 12:01:42 -0500

    fixed typo

--- 07-Compute-Engine/02_metadata_and_startup_scripts/bowtie-startup-final.sh
+++ 07-Compute-Engine/02_metadata_and_startup_scripts/bowtie-startup-final.sh
@@ -8,7 +8,7 @@ apt-get install -y apache2
 cat <<EOF> /var/www/html/index.html
 <body style="font-family: sans-serif">
 <html><body><h1>Aaaand.... Success!</h1>
-<p>My machine name is <span style="color: #3BA959">$NAME</span>$NAME<span> and I serve the <span style="color: #3BA959">$ENV</span> environment.</p>
+<p>My machine name is <span style="color: #3BA959">$NAME</span> and I serve the <span style="color: #3BA959">$ENV</span> environment.</p>
 <p>I live comfortably in the <span style="color: #5383EC">$ZONE</span> datacenter and proudly serve Tony Bowtie on the <span style="color: #D85040">$PROJECT</span> project.</p>
 <p><img src="https://storage.googleapis.com/tony-bowtie-pics/tony-bowtie.svg" alt="Tony Bowtie"></p>
 </body></html>
