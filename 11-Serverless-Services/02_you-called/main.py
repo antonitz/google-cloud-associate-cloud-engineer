ef7683bc1ea3151e93fbe370985069a26e7f7fbd
Antoni Tzavelas <antoni@Antonis-MBP.localdomain>
2020-12-08 13:19:09 -0500

    new files for function

---
+++ 11-Serverless-Services/02_you-called/main.py
@@ -0,0 +1,22 @@
+from flask import escape
+
+def you_called(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'lover of bowties'
+    return 'Hello {}! You called?'.format(escape(name))
