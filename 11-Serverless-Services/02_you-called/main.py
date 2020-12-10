e19a00d3e495fb979a366c226b9723f6e94061a6
Antoni Tzavelas <antoni@Antonis-MacBook-Pro.local>
2020-12-09 19:02:53 -0500

    small edits

--- 11-Serverless-Services/02_you-called/main.py
+++ 11-Serverless-Services/02_you-called/main.py
@@ -1,14 +1,7 @@
 from flask import escape
 
 def you_called(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """HTTP Cloud Function
     """
     request_json = request.get_json(silent=True)
     request_args = request.args
